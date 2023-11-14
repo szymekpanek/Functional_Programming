7560230a2d362316c5b2394037c562b6bd373d6b
szymekpanek20 <szymon.panek@partnerzyjutra.pl>
2023-11-14 12:24:31 +0100

    task 2_3 done

---
+++ src/ch_2/task2_3.java
@@ -0,0 +1,40 @@
+package ch_2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+//using arrayList write a program that puts 10 random number and sort them.
+public class task2_3 {
+    private void array_creator (ArrayList<Integer> data){
+        Random rand = new Random();
+        task2_3 task = new task2_3();
+
+        for (int i = 0; i < 100; i++) {
+            data.add(rand.nextInt(100));
+        }
+
+        System.out.println("Unsorted array:");
+        task.printArray(data);
+    }
+
+    private void merge_sort (ArrayList<Integer> data){
+        Collections.sort(data); //sorts the array
+        task2_3 task = new task2_3();
+
+        System.out.println("\n" + "Sorted array:");
+        task.printArray(data);
+
+    }
+
+    private void printArray(ArrayList<Integer> data) {
+        data.forEach((x) -> System.out.print(x + " "));
+    }
+
+    public static void main(String[] args) {
+        task2_3 task = new task2_3();
+        ArrayList<Integer> data = new ArrayList<>();
+        task.array_creator(data);
+        task.merge_sort(data);
+    }
+}
